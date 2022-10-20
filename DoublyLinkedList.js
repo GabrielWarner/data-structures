57471250c960d3ce95c993529411bff9152f68a1
GabrielWarner <gabe0412@uw.edu>
2022-10-20 16:49:44 -0700

    added insert

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -97,8 +97,21 @@ class DoublyLinkedList {
         }
         return false
     }
-
-
+    insert(index, val){
+        if(index < 0 || index > this.length) return false
+        if(index === 0) return !!this.unshift(val)
+        if(index === this.length) return !!this.push(val)
+        let newNode = new Node(val)
+        let prev = this.get(index - 1)
+        let third = prev.next
+        prev.next = newNode
+        newNode.prev = prev
+        newNode.next = third
+        third.perv = newNode
+        this.length++
+        return true
+    }
+    
 }
 
 const list = new DoublyLinkedList()
