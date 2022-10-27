8b242bebb60eb6a370f21f7369a93fe39f92e8c8
GabrielWarner <gabe0412@uw.edu>
2022-10-27 16:50:57 -0700

    BFS

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -34,7 +34,39 @@ class BinarySearchTree {
       }
     }
   }
+  find(val){
+    if(this.root === null) return false
+    let current = this.root
+    let found = false
+    while(!found && current){
+        if(val < current.value){
+            current = current.left
+        }else if(value > current.value){
+            current = current.right
+        }else{
+            found = true
+        }
+    }
+    if(!found == false)return undefined
+    return current
+  }
+  BFS(){
+    let node = this.root
+        data = [],
+        queue = [];
+        queue.push(node)
+
+        while(queue.length){
+            node = queue.shift()
+            data.push(node.value)
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+        return data
+  }
+  
 }
+
 let tree = new BinarySearchTree();
 tree.insert(10);
 tree.insert(5);
