6cbe200c81d68940fce64422927f5788effa6944
GabrielWarner <gabe0412@uw.edu>
2022-10-28 18:42:52 -0700

    added extraxtMax

--- Heap.js
+++ Heap.js
@@ -19,6 +19,43 @@ class MaxBinaryHeap{
         }
         return this.values
     }
+    extractMax(){
+        const max = this.values[0]
+        const end = this.values.pop()
+        if(this.values.length > 0){
+            this.values[0] = end
+            this.sinkDown()
+        }
+        return max
+    }
+    sinkDown(){
+        let index = 0
+        const length = this.values.length
+        const element = this.values[0]
+        while(true){
+            let leftChildIdx = 2 * index + 1
+            let rightChildIdx = 2 * index + 2
+            let leftChild,rightChild
+            let swap = null
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx]
+                if(leftChild>element){
+                    swap = leftChildIdx
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx]
+                if((swap === null && rightChild > element) ||(swap !== null && rightChild > leftChild)){
+                    swap =rightChildIdx
+            }
+        }
+        if(swap === null) break
+        this.values[index] = this.values[swap]
+        this.values[swap] = element
+        index = swap
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap()
@@ -31,5 +68,5 @@ heap.insert(44)
 heap.insert(34)
 heap.insert(67)
 heap.insert(112)
-
+heap.extractMax()
 console.log(heap)
