1b087c9e5e6fd6bf6f5bfc391f219f3e3a405c7c
GabrielWarner <gabe0412@uw.edu>
2022-10-28 17:44:39 -0700

    fixed heap bubble up

--- Heap.js
+++ Heap.js
@@ -1,19 +1,35 @@
 class MaxBinaryHeap{
-    constuctor(){
-        this.values = []
-    }
-    bubbleUp(){
-        let index = this.values.length-1
-        let parentIndex = Math.floor((index-1)/2)
-        while(this.values[parentIndex]<this.values[index]){
-            let temp = this.values[index]
-            this.values[index] = this.values[parentIndex]
-            this.values[parentIndex] = temp
-        }
-        return this.values
+    constructor(){
+        this.values = [];
     }
     insert(val){
         this.values.push(val)
-        this.bubbleUp()
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let index = this.values.length-1
+        const value = this.values[index]
+        while(index > 0){
+            let parentIndex = Math.floor((index-1)/2)
+            let parent = this.values[parentIndex]
+            if(value<=parent) break
+            this.values[parentIndex] = value
+            this.values[index] = parent
+            index = parentIndex
+        }
+        return this.values
     }
 }
+
+let heap = new MaxBinaryHeap()
+heap.insert(55)
+heap.insert(22)
+heap.insert(33)
+heap.insert(111)
+heap.insert(22)
+heap.insert(44)
+heap.insert(34)
+heap.insert(67)
+heap.insert(112)
+
+console.log(heap)
